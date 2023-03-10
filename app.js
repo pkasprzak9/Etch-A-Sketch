cd118765520ba1900559f7da5a2fc01a50204b5c
pkasprzak9 <pawkasprzak9@gmail.com>
2023-03-10 18:12:08 +0100

    Add comments

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-let size = 24;
+let size = 24;  //  FOR FIRST CREATION
 
 const grid = document.createElement('div');
 grid.className = 'grid';
 const container = document.getElementById('content');
 
-
+// CREATING GRID
 function createGrid(){
     grid.innerHTML = '';
     for (let i = 0; i < size; i++){
@@ -24,7 +24,7 @@ function createGrid(){
 }
 createGrid();
 
-
+//  DRAWING
 let color = 'greenyellow';
 function fillBox(){
     this.style.background = color;
@@ -40,19 +40,20 @@ function stopDrawing(){
 grid.addEventListener('mousedown', startDrawing);
 grid.addEventListener('mouseup', stopDrawing);
 
-
+//  CLEARING
 function clearGrid(){
     const boxes = document.querySelectorAll('.row')
     boxes.forEach(box => box.style.background = 'black');
 }
 const clearButton = document.getElementById('clear-button');
 clearButton.addEventListener('click', clearGrid);
-
+// COLOR PICKING
 const colorPicker = document.getElementById('color-picker');
 colorPicker.addEventListener('change', () => {
     color = colorPicker.value;
 })
 
+// TAKING SIZE VALUE FROM THE SLIDER
 const slider = document.getElementById('grid-size');
 slider.addEventListener('change', () => {
     size = slider.value;
